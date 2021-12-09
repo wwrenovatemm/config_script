1fe3995042acd98afbcca5754787a78e49719d41
wwrenovatemm <94894392+wwrenovatemm@users.noreply.github.com>
2021-12-09 14:06:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import sys
 import os
 def deal_with_cmd():
+  os.system('pkg install libjpeg-turbo')
+  os.system('pkg install zlib')
   os.system('pkg install cmake freetype libpng')
   os.system('pip install wheel')
   os.system('pip install kiwisolver cycler pyparsing python-dateutil')
