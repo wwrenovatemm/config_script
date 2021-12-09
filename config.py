b5a54c1e4fe1d8584a77a9d058007c9a98333568
wwrenovatemm <94894392+wwrenovatemm@users.noreply.github.com>
2021-12-09 13:47:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-echo "sshd" >> ~/.bashrc
+import sys
+import os
+def deal_with_cmd():
+  os.system('pkg install cmake freetype libpng')
+  os.system('pip install wheel')
+  os.system('pip install kiwisolver cycler pyparsing python-dateutil')
+  os.system('pip install matplotlib')
+  os.system('echo "sshd" >> ~/.bashrc')
+if __name__ == '__main__':
+    deal_with_cmd()
